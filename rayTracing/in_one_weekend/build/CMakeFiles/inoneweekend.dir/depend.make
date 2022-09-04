cf1ea1688031c6d6229160ddc3119c5aa3e6ea70
WuYichen <2950335254@qq.com>
2022-09-04 19:22:45 +0800

    del: delete build dir

--- rayTracing/in_one_weekend/build/CMakeFiles/inoneweekend.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for inoneweekend.
-# This may be replaced when dependencies are built.
--- rayTracing/in_one_weekend/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Graphics/Practice/rayTracing/in_one_weekend
-# Build directory: E:/Graphics/Practice/rayTracing/in_one_weekend/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
